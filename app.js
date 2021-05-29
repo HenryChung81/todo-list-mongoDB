3fd0a49c10580407d3b43f86c258038b8fe83062
Henry Chung <chungjenhao81@gmail.com>
2021-05-29 17:34:03 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
 const routes = require('./routes')
@@ -7,26 +6,10 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect('mongodb://localhost/todo-list3', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
 const port = 3000
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 // 在 app.engine 這一行我們只是在應用程式裡新增了一個叫 hbs 的樣板引擎，但要到 app.set 這一行，這個 hbs 元件才正式掛載到我們的主程式裡，開始啟用。
 
 app.use(routes)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const Todo = require('../../models/todo') // 載入 Todo model
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料，現在沒有傳入任何參數，所以會撈出整份資料。
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)) // 錯誤處理
 })
 
 module.exports = router
