4921476929cd082be312ddadc835b35d65f5cae4
Henry Chung <chungjenhao81@gmail.com>
2021-05-29 12:49:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
+mongoose.connect('mongodb://localhost/todo-list3', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 const port = 3000
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send(`Hello Express!`)
